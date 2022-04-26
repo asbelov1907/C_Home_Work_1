093b8ba1a185e928f8c2ed9e198d24d387efd609
AntonBelov <asbelov1907@gmail.com>
2022-04-26 08:25:00 +0300

    Добавлена первая задача

---
+++ Task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Напишите программу, которая на вход
+принимает два числа и выдаёт, какое число большее, а
+какое меньшее. */
+
+Console.Write("Введите число А - > ");
+double A = double.Parse(Console.ReadLine());
+
+Console.Write("Введите число B - > ");
+double B = double.Parse(Console.ReadLine());
+
+if(A>B) Console.Write("Max = " + A + " , Min = " + B);
+else if(A<B) Console.Write("Max = " + B + " , Min = " + A);
+else Console.Write("Оба числа равны");
