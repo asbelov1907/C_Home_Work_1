035045e388619754d2bd6a56c75037e7d696b38b
AntonBelov <asbelov1907@gmail.com>
2022-04-26 08:33:07 +0300

    Добавлена вторая задача

---
+++ Task_2/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.Write("Введите число А - > ");
+double a = double.Parse(Console.ReadLine());
+
+Console.Write("Введите число B - > ");
+double b = double.Parse(Console.ReadLine());
+
+Console.Write("Введите число C - > ");
+double c = double.Parse(Console.ReadLine());
+
+double max = a;
+
+if(b>max) max = b;
+if(c>max) max = c;
+
+Console.Write("Максимальное число = " + max);
